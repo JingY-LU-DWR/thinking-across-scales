0c494094128582223be002d7700105d0c1affa09
Jing Yuan <jing.yuan@water.ca.gov>
2026-07-16 21:09:01 -0700

    Restore working Mapbox configuration

--- assets/js/config.local.js
+++ assets/js/config.local.js
@@ -1,1 +1,1 @@
-export const MAPBOX_ACCESS_TOKEN = "pk.xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+export const MAPBOX_ACCESS_TOKEN = "pk.eyJ1IjoiamluZ3l1YW43NyIsImEiOiJjbXI1cDFydHAwcGt1MnhwenNuczU4dzY4In0.yxX1UuKzkjPBik0p0IF8HQ";
